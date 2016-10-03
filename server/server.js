d993399d1c9035d09de3374dd4e71c7452bf3db7
noamyogev84 <noamyogev84@gmail.com>
2016-10-03 14:26:57 +0300

    fixed server.js static files directory

--- server/server.js
+++ server/server.js
@@ -1,29 +1,31 @@
 'use strict';
 
-var loopback = require('loopback');
-var boot = require('loopback-boot');
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var favicon = require('serve-favicon');
+var morgan = require('morgan'); // formerly express.logger
+var errorhandler = require('errorhandler');
+var app = express();
 
-var app = module.exports = loopback();
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
 
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Noams Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
-};
+// express/connect middleware
+//what the fuck is this?
+//app.use(favicon(__dirname + '/favicon.ico'));
+app.use(morgan('dev'));
 
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
+// serve up static assets
+app.use(express.static('client'));
 
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+// development only
+if ('development' === app.get('env')) {
+  app.use(errorhandler());
+}
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('noam the kings server listening on port ' + app.get('port'));
 });
